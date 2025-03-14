934fad4414ba2a2a935b7f1450bfe40458be4cff
t321nagatan <t321nagatan@gmail.com>
2025-03-14 21:56:06 +0900

    一覧を追加

--- FullApp3/FullApp3/ViewModels/MainWindowViewModel.cs
+++ FullApp3/FullApp3/ViewModels/MainWindowViewModel.cs
@@ -7,15 +7,23 @@ namespace FullApp3.ViewModels
     public class MainWindowViewModel : BindableBase
     {
         public DelegateCommand NavigateToTimeCardCommand { get; private set; }
+        public DelegateCommand NavigateToTimeCardListCommand { get; private set; }
 
         public MainWindowViewModel()
         {
             NavigateToTimeCardCommand = new DelegateCommand(ExecuteNavigateToTimeCard);
+            NavigateToTimeCardListCommand = new DelegateCommand(ExecuteNavigateToTimeCardList);
         }
 
         private void ExecuteNavigateToTimeCard()
         {
             var window = new EditTimeCard();
+            window.Show();
+        }
+
+        private void ExecuteNavigateToTimeCardList()
+        {
+            var window = new TimeCardList();
             window.Show();
         }
 
---
+++ FullApp3/Modules/FullApp3.Modules.ModuleName/Views/TimeCardList.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace FullApp3.Modules.TimeCard.Views
+{
+    public partial class TimeCardList : Window
+    {
+        public TimeCardList()
+        {
+            InitializeComponent();
+        }
+    }
+} 
